0d98f575d710cfca46390d05ea06db126eab49c1
CameronDunning <dunningcameron@gmail.com>
2019-10-16 18:36:38 +0000

    Add order details to order page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,8 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
+    @line_item = LineItem.where("order_id = #{params[:id]}")
+    # throw @line_item.inspect
   end
 
   def create
